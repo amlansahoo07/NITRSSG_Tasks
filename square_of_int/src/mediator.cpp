345156d439e8452dc8cc4bf5edb5f51b0f2423be
Amlan Sahoo <35356517+amlansahoo07@users.noreply.github.com>
2020-10-01 18:56:10 +0530

    Commit modified files

---
+++ square_of_int/src/mediator.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include <sstream>
+#include <boost/bind.hpp>
+
+ros::NodeHandle* n;
+
+void processCallback(const std_msgs::Int32::ConstPtr& msg)
+{
+  ROS_INFO("Squaring the number: [%d]", msg->data*msg->data);
+  std_msgs::Int32 msg2;
+  msg2.data = msg->data*msg->data;
+  ros::Publisher pub = n->advertise<std_msgs::Int32>("chatter", 1000);
+  pub.publish(msg2);
+  ros::spinOnce(); 
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "mediator");
+  n = new ros::NodeHandle;
+  ros::Subscriber sub = n->subscribe<std_msgs::Int32>("process", 1000, processCallback);
+  ros::spin();
+
+  return 0;
+}
